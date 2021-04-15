5745512e269cdcd59bbf5e873efd90c7e9d50cde
Azeezat Oladunni <tomisinoladunni501@gmail.com>
2021-04-16 00:47:13 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>basic calculations</title>
+</head>
+<body>
+<?php
+$a = 6;
+$b = 4;
+$c = $a + $b;
+echo "Addition operation result: $c <br/>";
+$c = $a - $b;
+echo "Substraction operation result: $c <br/>";
+$c = $a / $b;
+echo "Division operation result: $c <br/>";
+$c = $a * $b;
+echo "Multiplication operation result: $c <br/>";
+$c = $a % $b;
+echo "Modulus operation result: $c <br/>";
+
+?>
+</body>
+</html>
